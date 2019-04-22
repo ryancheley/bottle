5cef70a34b6279b28a3e653a5ed992290bdf0c9f
Ryan Cheley <rcheley@gmail.com>
2019-04-21 19:03:04 -0700

    updated developer name

--- sample_app.py
+++ sample_app.py
@@ -16,7 +16,7 @@ def handle_root_url():
 @route('/profile')
 def make_request():
     # make an API request here
-    profile_data = {'name': 'Marcel Hellkamp', 'role': 'Developer'}
+    profile_data = {'name': 'Ryan Cheley', 'role': 'Developer'}
     return template('details', data=profile_data)
 
 
